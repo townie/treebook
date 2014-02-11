3a7bd2d45a2c208457de58daca1e4004dfd0021c
keith.e.webber <keith.e.webber@gmail.com>
2014-02-11 15:38:07 -0500

    Added test for user first_name, last_name and profile correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,16 @@ class User < ActiveRecord::Base
 
   has_many :statuses
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile, presence: true,
+     uniqueness: true,
+     format: {
+        with: /a-zA-Z0-9_-/,
+        message: 'Must be formated correctly.'
+     }
+
+
 
   def full_name
     first_name + " " + last_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,40 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a user should enter first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+  test "a user should enter profle name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile].empty?
+  end
+
+  test "a user should have a unique profile name" do
+  	user = User.new
+  	user.profile = users(:jason).profile
+  	
+  	assert !user.save
+  	assert !user.errors[:profile].empty?
+  end
+
+  test "a user should have a profile without spaces" do
+  	user = User.new
+  	user.profile = "My Prfole with Spaces"
+
+  	assert !user.save
+  	assert !user.errors[:profile].empty?
+  	assert user.errors[:profile].include?("Must be formated correctly.")
+  end
+
+
 end
